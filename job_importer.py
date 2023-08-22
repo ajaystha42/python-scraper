b0842819c2e5211b242176673675b7ea4b581943
Ajay Shrestha <er.ajaystha@gmail.com>
2023-08-22 14:26:46 -0400

    fetch each companys job using apis

--- job_importer.py
+++ job_importer.py
@@ -10,9 +10,27 @@ from bs4 import BeautifulSoup
 
 #  Running Code
 # base_target_url = 'https://www.linkedin.com/jobs/netflix-jobs-worldwide?keywords=Netflix&location=Worldwide&locationId=&geoId=92000000&f_TPR=r604800&f_C=165158&position=1&pageNum=0'
+def fetch_job_ids(total_jobs, query_params):
+    linked_in_job_search_url = f'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?{query_params}'
+    job_ids = set()
+    for i in range(0, math.ceil(total_jobs/25)):
+        # pagination
+        target_url = f'{linked_in_job_search_url}&start={i * 25}'
+        res = requests.get(target_url)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        alljobs_on_this_page = soup.find_all('li')
+        for x in range(0, len(alljobs_on_this_page)):
+            base_card = alljobs_on_this_page[x].find(
+                'div', {'class': 'base-card'})
+            if base_card is not None:
+                jobid = base_card.get('data-entity-urn')
+                if jobid is not None:
+                    jobid = jobid.split(':')[3]
+                    job_ids.add(jobid)
+    return list(job_ids)
 
 
-def import_jobs(company_code, jobids):
+def import_jobs(company_code):
     # Using Enhanced URL 1
     base_target_url = f'https://www.linkedin.com/jobs/search?position=1&pageNum=0&f_C={company_code}&f_TPR=r604800&geoId=92000000'
     main_res = requests.get(base_target_url)
@@ -23,74 +41,9 @@ def import_jobs(company_code, jobids):
         total_jobs_text = total_jobs_span.get_text()
         total_jobs_count = ''.join(filter(str.isdigit, total_jobs_text))
         print('Total jobs count:', total_jobs_count)
-        # jobids = set()
         n = pd.to_numeric(total_jobs_text)
         query_params = base_target_url.split('?')[1]
-        linked_in_job_search_url = f'https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?{query_params}'
-        link_url = 'https://www.linkedin.com/jobs/view/'
-
-        for i in range(0, math.ceil(n/25)):
-            # pagination
-            target_url = f'{linked_in_job_search_url}&start={i * 25}'
-            res = requests.get(target_url)
-            soup = BeautifulSoup(res.text, 'html.parser')
-            alljobs_on_this_page = soup.find_all('li')
-            for x in range(0, len(alljobs_on_this_page)):
-                base_card = alljobs_on_this_page[x].find(
-                    'div', {'class': 'base-card'})
-                if base_card is not None:
-                    jobid = base_card.get('data-entity-urn')
-                    if jobid is not None:
-                        jobid = jobid.split(':')[3]
-                        jobids.add(jobid)
-        job_details = []
-        for id in jobids:
-            obj = {}
-            print(f'Fetching info for job id : {id}')
-            obj['job_id'] = id
-            job_url = f'https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/{id}'
-            # job_url = f'https://www.linkedin.com/jobs/view/{id}'
-            res = requests.get(job_url)
-            soup = BeautifulSoup(res.text, 'html.parser')
-            title = soup.find(
-                'h2', class_='top-card-layout__title')
-            # title = soup.find(
-            #     'h1', class_='t-24 t-bold jobs-unified-top-card__job-title')
-            # title = soup.find(
-            #     'div', {'class': 'top-card-layout__entity-info'}).find('a').text.strip()
-            if title:
-                title = title.get_text().strip()
-                obj['title'] = title
-                organization_name = soup.find(
-                    'a', class_='topcard__org-name-link')
-                # organization_name = soup.find(
-                #     'div', {'class': 'top-card-layout__card'}).find('a').find('img').get('alt')
-                if organization_name:
-                    company = organization_name.get_text().strip()
-                    obj['company'] = company
-                    job_description = soup.find(
-                        'div', class_='show-more-less-html__markup')
-                    if job_description:
-                        job_description = job_description.get_text().strip()
-                        obj['job_description'] = job_description
-                        image = soup.find(
-                            'img', class_='artdeco-entity-image artdeco-entity-image--square-5')
-                        if image:
-                            image_url = image.get('data-delayed-url')
-                            obj['link_url'] = link_url + id
-                            obj['image'] = image_url
-                            job_details.append(obj)
-                    else:
-                        print('job description not found')
-                else:
-                    print('organization not found')
-            else:
-                print('title not found')
-        # jobids = []
-        # update in db
-        df = pd.DataFrame(job_details)
-        df.to_csv(f'jobs_{company_code}_scraping.csv',
-                  index=False, encoding='utf-8')
-        print(len(job_details), ' jobs imported.')
+        job_ids = fetch_job_ids(n, query_params)
+        return job_ids
     else:
         print('Total jobs span not found.')
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import re
 import job_importer
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
 
 
 '''
@@ -30,7 +33,10 @@ companies = ['netflix', 'fusemachines',
 options = Options()
 # options.add_experimental_option("detach", True)
 
-
+proxy_host = ''
+proxy_username = ''
+proxy_password = ''
+proxy_port = 123
 # Proxy Bright Data Configuration
 # options.add_argument('--proxy-server=http://%s:%s@%s:%s' %
 #                      (proxy_username, proxy_password, proxy_host, str(proxy_port)))
@@ -39,11 +45,11 @@ options = Options()
 
 
 # Run Chrome in headless mode
-# options.add_argument('--headless')
+options.add_argument('--headless')
 
 
 # Disable GPU acceleration
-# options.add_argument('--disable-gpu')
+options.add_argument('--disable-gpu')
 
 driver = webdriver.Chrome(
     # service=service,
@@ -56,13 +62,17 @@ driver.implicitly_wait(10)
 #     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
 #     'Accept-Language': 'en-US,en;q=0.9',
 # }
+
+# total_jobs = [['3698260330', '3701051286', '3691231651', '3698256616',
+#               '3698262028', '3698260328', '3698255995', '3698255996']]
+
+total_jobs = list()
+url = 'https://www.linkedin.com/company'
 for company in companies:
     try:
-        jobids = set()
-
         print(f'Fetching Job Informations for Company : {company}')
         # driver.get(f'https://www.linkedin.com/company/{company}/jobs/')
-        driver.get(f'https://www.linkedin.com/company/{company}/')
+        driver.get(f'{url}/{company}/')
 
         modal_close_button = driver.find_element(
             "xpath", "//button[@aria-label='Dismiss']")
@@ -74,13 +84,58 @@ for company in companies:
         if company_tag:
             if match:
                 f_c_value = match.group(1)
-                print("Company Code :: ", f_c_value)
                 # Testing job_importer code
-                job_importer.import_jobs(f_c_value, jobids)
+                jobids = job_importer.import_jobs(f_c_value)
+                total_jobs.append(jobids)
         else:
             print('No See Jobs Element')
     except:
         print(f'No jobs for {company}')
 
-# Now we can use f_c value to fetch all jobs and then
-#  use the job ids to fetch its jobs description
+job_details = []
+link_url = 'https://www.linkedin.com/jobs/view/'
+jobs = [
+    element for inner_array in total_jobs for element in inner_array]
+for id in jobs:
+    obj = {}
+    print(f'Fetching info for job id : {id}')
+    obj['job_id'] = id
+    job_url = f'https://www.linkedin.com/jobs-guest/jobs/api/jobPosting/{id}'
+    # job_url = f'https://www.linkedin.com/jobs/view/{id}'
+    res = requests.get(job_url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    title = soup.find(
+        'h2', class_='top-card-layout__title')
+    if title:
+        title = title.get_text().strip()
+        obj['title'] = title
+        organization_name = soup.find(
+            'a', class_='topcard__org-name-link')
+        if organization_name:
+            company = organization_name.get_text().strip()
+            obj['company'] = company
+            job_description = soup.find(
+                'div', class_='show-more-less-html__markup')
+            if job_description:
+                job_description = job_description.get_text().strip()
+                obj['job_description'] = job_description
+                image = soup.find(
+                    'img', class_='artdeco-entity-image artdeco-entity-image--square-5')
+                if image:
+                    image_url = image.get('data-delayed-url')
+                    obj['link_url'] = link_url + id
+                    obj['image'] = image_url
+                    job_details.append(obj)
+            else:
+                print('job description not found')
+        else:
+            print('organization not found')
+    else:
+        print('title not found')
+        # jobids = []
+        # update in db
+print(len(job_details))
+df = pd.DataFrame(job_details)
+df.to_csv(f'linkedin_jobs.csv',
+          index=False, encoding='utf-8')
+print(len(job_details), ' jobs imported.')
